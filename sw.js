39fc512a17643a6e71488372c8bf877db041b60a
streetsevo <streetsevo@gmail.com>
2026-09-17 20:02:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Ekstruzja - offline cache. Bump CACHE on every release. */
-const CACHE = "ekstruzja-v50";
+const CACHE = "ekstruzja-v51";
 const FILES = ["./", "./index.html", "./manifest.json",
                "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
